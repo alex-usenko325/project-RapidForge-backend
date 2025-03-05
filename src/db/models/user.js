d444f3e2d2861d0584395f8c937a10d41c548e0c
Nastja Demchyk <NastjaDemchyk@gmail.com>
2025-03-05 17:23:59 +0100

    added user.js to controllers, services, routers, added validation schema for userupdate and expanded userShema

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -2,9 +2,18 @@ import { model, Schema } from 'mongoose';
 
 const usersSchema = new Schema(
   {
-    name: { type: String, required: true },
+    name: { type: String, required: true, default: '' },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    gender: { type: String, enum: ['woman', 'man'], default: 'woman' },
+    weight: { type: Number, default: 0 },
+    dailySportTime: { type: Number, default: 0 },
+    dailyNorm: { type: Number, default: 1500, min: 500, max: 15000 },
+    avatar: {
+      type: String,
+      default:
+        'https://res.cloudinary.com/diee7l1on/image/upload/v1741187439/default-avatar-profile-icon-social-media-user-image-gray-avatar-icon-blank-profile-silhouette-vector-illustration_561158-3467_bvqe8k.avif',
+    },
   },
   { timestamps: true, versionKey: false },
 );
